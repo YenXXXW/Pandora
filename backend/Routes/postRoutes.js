417b5ba03369476aa3f18479ab478764668ca259
yenxxxw <yenxxxw@gmail.com>
2024-05-24 11:53:23 +0630

    backend cookie

--- backend/Routes/postRoutes.js
+++
@@ -1,8 +0,0 @@
-import * as postControllers from "../controllers/posts"
-import express from "express"
-
-const router = express.Router()
-
-router.get("/posts", postControllers.getPosts);
-
-export default router
